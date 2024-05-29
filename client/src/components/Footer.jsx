ce9602c9caa118b5fbaffb0edee68227563a2ed5
AslanHalil <AslanCHalil@outlook.com>
2024-05-29 19:19:21 +0300

    Fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Logo from "../img/logo.png";
+
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <img src={Logo} alt=""/>
-            <span>Made with <b>React.js</b>.</span>
-        </div>
-    )
-}
-
-export default Footer
+            <span>Made with ♥️ and <b>React.js</b>.</span>
+        </footer>
+    );
+};
+export default Footer;

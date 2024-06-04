3354590fa7239bbee5ac8ffcc3eaea4539cbf1d8
AslanHalil <AslanCHalil@outlook.com>
2024-06-04 14:28:10 +0300

    displaying and navigation of post categories.

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {getPosts} from "../controllers/post.js";
+import {deletePost, getPost, getPosts} from "../controllers/post.js";
 
 const router = express.Router()
 
 router.get("/", getPosts)
-// router.get("/:id", getPost)
+router.get("/:id", getPost)
 // router.post("/", addPost)
-// router.delete("/:id", deletePost)
+router.delete("/:id", deletePost)
 // router.put("/:id", updatePost())
 
 export default router;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,61 +1,70 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import Menu from "../components/Menu.jsx";
-
+import axios from "axios";
+import moment from "moment";
+import {AuthContext} from "../context/authContext.jsx";
 
 const Single = () => {
+
+    const [post, setPost] = useState({});
+
+    const location = useLocation()
+
+    const navigate = useNavigate();
+
+    const postId = location.pathname.split("/")[2];
+
+    const {currentUser} = useContext(AuthContext);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await axios.get(`http://localhost:8800/api/posts/${postId}`);
+                navigate("/")
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, [postId]);
+
+    const handleDelete = async ()=> {
+        try {
+            const res = await axios.delete(`http://localhost:8800/api/posts/${postId}`);
+            navigate("/")
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="single">
             <div className="content">
                 <img
-                    src="https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    src={post?.img}
                     alt=""/>
                 <div className="user">
-                    <img
-                        src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt=""/>
+                    {post.userImg && <img
+                        src={post.userImg}
+                        alt=""/>}
                     <div className="info">
-                        <span>Aslan</span>
-                        <p>Posted 2 days ago</p>
+                        <span>{post.username}</span>
+                        <p>Posted {moment(post.date).fromNow()}</p>
                     </div>
-                    <div className="edit">
+                    {currentUser.username === post.username && (<div className="edit">
                         <Link to={`/write?edit=2`}>
                             <img src={Edit}/>
                         </Link>
-                        <img src={Delete}/>
-                    </div>
+                        <img onClick={handleDelete} src={Delete}/>
+                    </div>)}
                 </div>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nibh eleifend,
-                    vestibulum velit non, varius est.</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nibh eleifend,
-                    vestibulum velit non, varius est. Vestibulum sit amet auctor ipsum. Pellentesque eu nibh quis orci
-                    laoreet viverra nec et lectus. Curabitur luctus eros sed purus semper, quis pellentesque orci
-                    euismod. Quisque feugiat commodo dolor vitae lobortis. In quam quam, luctus in tristique non,
-                    porttitor a nisi. Proin condimentum justo eu erat aliquet cursus et ac magna. Etiam consequat ipsum
-                    sit amet ligula blandit feugiat. Nunc vitae odio id augue convallis molestie.
-
-                    Aliquam sed neque et felis tempus sollicitudin. Etiam ultrices justo ac eros varius, at aliquam elit
-                    mattis. Proin cursus, erat vel congue dignissim, massa justo pharetra lorem, eu interdum orci ex
-                    ornare quam. Praesent egestas purus sit amet placerat interdum. Donec scelerisque enim orci, commodo
-                    dictum ante dignissim ut. Vestibulum fringilla, leo et molestie eleifend, metus odio dapibus ipsum,
-                    sit amet tincidunt dolor turpis non neque. Praesent tincidunt pharetra finibus. Suspendisse placerat
-                    pulvinar turpis eget varius. Nam mollis dictum pulvinar. Cras eu tellus rutrum, maximus quam in,
-                    posuere velit. Suspendisse potenti. Suspendisse pulvinar metus mi, et pharetra tellus pellentesque
-                    feugiat. Nulla tristique dapibus auctor. Pellentesque habitant morbi tristique senectus et netus et
-                    malesuada fames ac turpis egestas. Fusce lobortis pretium eros, vitae bibendum magna efficitur et.
-                    Quisque ut facilisis augue, sit amet laoreet sapien.
-
-                    Pellentesque vestibulum tincidunt massa vel consequat. Quisque viverra turpis eu nulla blandit, in
-                    vehicula sapien malesuada. Cras hendrerit aliquet tortor non placerat. Sed quis libero non nisi
-                    dapibus imperdiet vitae eget felis. Vivamus mollis pharetra accumsan. Vestibulum pulvinar cursus
-                    purus, congue blandit tellus facilisis id. Vivamus ipsum diam, hendrerit at nunc in, mollis egestas
-                    mi. Nam efficitur egestas neque, nec auctor nulla vulputate eu. Vivamus porta, orci ac faucibus
-                    convallis, nisl neque finibus eros, a finibus enim ex accumsan velit. Duis at eleifend risus.
-                </p>
+                <h1>{post.title}</h1>
+                {post.desc}
             </div>
-            <Menu/>
+            <Menu cat={post.cat}/>
         </div>
     )
 }
